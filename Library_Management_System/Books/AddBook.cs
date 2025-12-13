502278c56a01b821395a642c5e152092d0efacb7
hafizecim <hfz.snyl@gmail.com>
2025-12-13 20:39:52 +0300

    DataBase.cs veri tabanı için dosya eklendi

--- Library_Management_System/Books/AddBook.cs
+++ Library_Management_System/Books/AddBook.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -32,9 +33,10 @@ namespace Library_Management_System.Books
 
         private void roundedButtonControl1_Click(object sender, EventArgs e)
         {
-
+            
+            
         }
-
+        
         private void gradientPanel1_Paint(object sender, PaintEventArgs e)
         {
 
---
+++ Library_Management_System/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    public class Database
+    {
+        public static string baglantiCumlesi = @"Server=.;Database=VTkutuphane;Integrated Security=True;";
+
+
+        public static SqlConnection GetSqlConnection()
+        {
+            try
+            {
+                SqlConnection baglanti = new SqlConnection(baglantiCumlesi);
+                baglanti.Open();
+                Console.WriteLine("Bağlantı Kuruldu");
+                return baglanti;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bağlantı Hatası: " + ex.Message);
+                //return null;  // Hata olursa null döndür
+                throw;
+            }
+        }
+    }
+}

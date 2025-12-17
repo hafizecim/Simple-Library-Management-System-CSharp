f5e87d40379ba3195bf674a17d04f78e06051d11
hafizecim <hfz.snyl@gmail.com>
2025-12-17 21:16:54 +0300

    ViewBook form sayfası oluşturuldu

--- Library_Management_System/Books/ViewBooks.Designer.cs
+++ Library_Management_System/Books/ViewBooks.Designer.cs
@@ -31,6 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.gradientPanel1 = new Library_Management_System.GradientPanel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGeri = new Library_Management_System.RoundedButtonControl();
+            this.gradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // gradientPanel1
@@ -38,6 +43,9 @@
             this.gradientPanel1.Angle = 50F;
             this.gradientPanel1.BackColor = System.Drawing.Color.Indigo;
             this.gradientPanel1.BottomColor = System.Drawing.Color.Indigo;
+            this.gradientPanel1.Controls.Add(this.btnGeri);
+            this.gradientPanel1.Controls.Add(this.dataGridView1);
+            this.gradientPanel1.Controls.Add(this.label1);
             this.gradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gradientPanel1.Location = new System.Drawing.Point(0, 0);
             this.gradientPanel1.Name = "gradientPanel1";
@@ -50,6 +58,48 @@
             this.timer1.Interval = 2000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(283, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitapları Görüntüle";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(55, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(699, 232);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.BackColor = System.Drawing.Color.Transparent;
+            this.btnGeri.BorderColor = System.Drawing.Color.White;
+            this.btnGeri.BorderRadius = 40;
+            this.btnGeri.BorderSize = 2;
+            this.btnGeri.ButtonText = "";
+            this.btnGeri.FlatAppearance.BorderSize = 0;
+            this.btnGeri.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGeri.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGeri.ForeColor = System.Drawing.Color.White;
+            this.btnGeri.HoverBackColor = System.Drawing.Color.Violet;
+            this.btnGeri.Location = new System.Drawing.Point(505, 348);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.NormalBackColor = System.Drawing.Color.Transparent;
+            this.btnGeri.Size = new System.Drawing.Size(249, 90);
+            this.btnGeri.TabIndex = 10;
+            this.btnGeri.Text = "Geri";
+            this.btnGeri.UseVisualStyleBackColor = false;
+            // 
             // ViewBooks
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -58,6 +108,9 @@
             this.Controls.Add(this.gradientPanel1);
             this.Name = "ViewBooks";
             this.Text = "ViewBooks";
+            this.gradientPanel1.ResumeLayout(false);
+            this.gradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -66,5 +119,8 @@
 
         private GradientPanel gradientPanel1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private RoundedButtonControl btnGeri;
     }
 }
